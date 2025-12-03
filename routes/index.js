326026abeccdbb70169cdf0ae319a5aa0f1edfb8
Antonio Cordero <antoniocorderomn@gmail.com>
2025-12-03 13:17:17 +0100

    3/12/25

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const dataService = require('../data/dataService');
+
+//lista de todos los pokemon
+router.get('/', function (req, res, next) {
+  const pokemons = dataService.findAllPokemons();
+  const tipos = Array.from(dataService.findAllPokemonTypes());
+
+  res.render('index', { pokemons, tipos, tipoSeleccionado: 'Todos' });
+});
+
+//pokemon por id
+router.get('/pokemon/:id', function (req, res, next) {
+  const id = req.params.id;
+  const pokemon = dataService.findPokemonById(id);
+  const tipos = Array.from(dataService.findAllPokemonTypes());
+
+  if (!pokemon) {
+    return res.status(404).send('Pokémon no encontrado');
+  }
+
+  res.render('detalle', { pokemon, tipos });
+});
+
+//filtrar por tipo
+router.get('/tipo/:tipo', function (req, res, next) {
+  const tipo = req.params.tipo;
+  const pokemons = dataService.findAllPokemonsByType(tipo);
+  const tipos = Array.from(dataService.findAllPokemonTypes());
+
+  res.render('index', { pokemons, tipos, tipoSeleccionado: tipo });
 });
 
 module.exports = router;
